48278667bb94415d20ca0c9ff0ba46d03c67a7ef
lerrybe <lerry597@gmail.com>
2022-06-10 18:07:17 +0900

    chore: 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
+
 export default function App() {
-  return <div>Hello venture!</div>;
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/*' element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
 }
---
+++ src/pages/NotFoundPage/index.js
@@ -0,0 +1,3 @@
+export default function NotFoundPage() {
+  return <div>잘못된 요청입니다. 돌아가시우</div>;
+}
