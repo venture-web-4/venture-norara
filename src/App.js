57ad6460ac10bebbc493e054a6b5783a747d358d
Bin <dbswoqls89@snu.ac.kr>
2022-06-13 16:22:53 +0900

    structuring tetris

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 
-import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
+import Index from './pages/Tetris';
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        <Route path='/' element={<Index />} />
         <Route path='/*' element={<NotFoundPage />} />
       </Routes>
     </>
---
+++ src/pages/Tetris/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Tetris from '../../components/Tetris'
+
+const Index = () => (
+  <div className="Index">
+    <Tetris/>
+  </div>
+)
+
+export default Index;
