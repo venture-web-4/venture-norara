b0f32a7d0e86711c044596875aa63f024dcd3ba5
Bin <dbswoqls89@snu.ac.kr>
2022-06-14 20:26:13 +0900

    error recurred & changed item

--- src/hooks/usePreventLeave.js
+++
@@ -1,9 +0,0 @@
-export const usePreventLeave = () => {
-  const listener = e => {
-    e.preventDefault();
-    e.returnValue = '';
-  };
-  const checkLeave = () => window.addEventListener('beforeunload', listener);
-
-  return { checkLeave };
-};
