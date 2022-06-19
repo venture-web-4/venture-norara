bf2c2af671b89a2095e2de32fc8af8031fd86b0d
lerrybe <lerry597@gmail.com>
2022-06-20 04:16:33 +0900

    feat: 랜딩 페이지 UI + 컴포넌트 구현

--- src/components/LandingHeader/LandingHeader.styled.js
+++ src/components/LandingHeader/LandingHeader.styled.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 import '../../styles/landing.scss';
+import '../../styles/font.scss';
 
 export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
   position: absolute;
   top: 43%;
   left: 50%;
@@ -74,3 +77,129 @@ export const Announce = styled.div`
 `;
 
 export const ButtonWrapper = styled.div``;
+
+export const DescriptionWrapper = styled.div`
+  margin-right: 2.5vw;
+  margin-left: 3vw;
+`;
+
+export const RankingWrapper = styled.div`
+  width: auto;
+  padding: 30px 25px;
+  background-color: rgba(255, 255, 255, 0.1);
+  border-radius: 30px;
+  height: auto;
+  font-family: 'DungGeunMo';
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const RankingTitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: cetner;
+  margin-bottom: 20px;
+`;
+
+export const RankingImg = styled.div`
+  background-image: url('../../../img/question.gif');
+  width: 35px;
+  height: 35px;
+  background-size: 35px 35px;
+`;
+
+export const RankingTitle = styled.div`
+  font-size: 30px;
+  margin: 0 15px;
+  font-weight: 600;
+`;
+
+export const RankingEachGameWrapper = styled.div`
+  width: auto;
+  height: auto;
+  display: flex;
+`;
+
+export const GameTitleRankersWrapper = styled.div`
+  width: auto;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  margin-right: 10px;
+
+  &:last-child {
+    margin-right: 0;
+  }
+`;
+
+export const GameTitle = styled.div`
+  background-color: rgba(255, 255, 255, 0.5);
+  margin-bottom: 15px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 21px;
+  height: 40px;
+`;
+
+export const RankingEachGame = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: rgba(255, 255, 255, 0.32);
+  width: 190px;
+  min-height: 324.5px;
+  border-radius: 10px;
+  padding: 10px 0px 10px 15px;
+`;
+
+export const Rankers = styled.div`
+  line-height: 30px;
+  font-size: 15px;
+  color: #898989;
+  font-weight: 600;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  &:first-child {
+    font-size: 21px;
+    color: #ffcf40;
+    text-shadow: -1px 0 #000, 0 1px #000, 1px 0 #000, 0 -1px #000;
+  }
+  &:nth-child(2) {
+    font-size: 19px;
+    color: #c0c0c0;
+    text-shadow: -1px 0 #000, 0 1px #000, 1px 0 #000, 0 -1px #000;
+  }
+  &:nth-child(3) {
+    font-size: 17px;
+    color: #cc9553;
+    text-shadow: -1px 0 #000, 0 1px #000, 1px 0 #000, 0 -1px #000;
+  }
+`;
+
+export const Score = styled.span`
+  margin-left: 3px;
+  font-size: 15px;
+`;
+
+export const Loading = styled.div`
+  color: #424242;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  width: 100%;
+  height: 100%;
+`;
+
+export const LoadingImg = styled.div`
+  background-image: url('../../../img/loading.gif');
+  width: 65px;
+  height: 50px;
+  background-size: 65px 50px;
+  margin-right: 15px;
+`;
--- src/components/LandingHeader/index.js
+++ src/components/LandingHeader/index.js
@@ -4,26 +4,193 @@ import {
   AnnuonceWrapper,
   ButtonWrapper,
   SubTitleWrapper,
+  DescriptionWrapper,
+  RankingWrapper,
   TitleOuterWrapper,
   TitleWrapper,
   Wrapper,
+  RankingTitleWrapper,
+  RankingTitle,
+  RankingEachGameWrapper,
+  RankingEachGame,
+  RankingImg,
+  Rankers,
+  GameTitleRankersWrapper,
+  GameTitle,
+  Score,
+  Loading,
+  LoadingImg,
 } from './LandingHeader.styled';
 
 export default function LandingHeader() {
+  // MOCK DATA: 게임별 score 필터링해서 랭크 보여주기
+  const rankersOnUFOGame = [
+    ['4조', 600],
+    ['페이커', 500],
+    ['홍진호', 400],
+    ['2등진호', 300],
+    ['버그픽서', 299],
+    ['으라차차빈재윤', 200],
+    ['기몌지', 100],
+    ['욱병최!', 20],
+    ['내가 게임의신', 10],
+    ['정정정', 5],
+  ];
+
+  const rankersOnGeoguesser = [
+    ['4조', 600],
+    ['페이커', 500],
+    ['홍진호', 400],
+    ['2등진호', 300],
+    ['버그픽서', 299],
+    ['으라차차빈재윤', 200],
+    ['기몌지', 100],
+    ['욱병최!', 20],
+    ['내가 게임의신', 10],
+    ['정정정', 5],
+  ];
+
+  const rankersOnCatGame = undefined;
+
+  // const rankersOnCatGame = [
+  //   ['4조', 600],
+  //   ['페이커', 500],
+  //   ['홍진호', 400],
+  //   ['2등진호', 300],
+  //   ['버그픽서', 299],
+  //   ['으라차차빈재윤', 200],
+  //   ['기몌지', 100],
+  //   ['욱병최!', 20],
+  //   ['내가 게임의신', 10],
+  //   ['정정정', 5],
+  // ];
+
+  const rankersOnAsteroids = [
+    ['4조', 600],
+    ['페이커', 500],
+    ['홍진호', 400],
+    ['2등진호', 300],
+    ['버그픽서', 299],
+    ['으라차차빈재윤', 200],
+    ['기몌지', 100],
+    ['욱병최!', 20],
+    ['내가 게임의신', 10],
+    ['정정정', 5],
+  ];
+
   return (
     <Wrapper>
-      <TitleOuterWrapper>
-        <SubTitleWrapper>{'심심할 때'}</SubTitleWrapper>
-        <TitleWrapper>{'NORARA'}</TitleWrapper>
-      </TitleOuterWrapper>
+      <DescriptionWrapper>
+        <TitleOuterWrapper>
+          <SubTitleWrapper>{'심심할 때'}</SubTitleWrapper>
+          <TitleWrapper>{'NORARA'}</TitleWrapper>
+        </TitleOuterWrapper>
 
-      <AnnuonceWrapper>
-        <Announce>{'🕹 시간 순삭되는 다양한 게임에 참여해보세요.💎'}</Announce>
-      </AnnuonceWrapper>
+        <AnnuonceWrapper>
+          <Announce>{'🕹 시간 순삭되는 다양한 게임에 참여해보세요.💎'}</Announce>
+        </AnnuonceWrapper>
 
-      <ButtonWrapper>
-        <LandingButton text={'GET STARTED'} />
-      </ButtonWrapper>
+        <ButtonWrapper>
+          <LandingButton text={'GET STARTED'} />
+        </ButtonWrapper>
+      </DescriptionWrapper>
+
+      <RankingWrapper>
+        <RankingTitleWrapper>
+          <RankingImg />
+          <RankingTitle>명예의 전당</RankingTitle>
+          <RankingImg />
+        </RankingTitleWrapper>
+
+        <RankingEachGameWrapper>
+          {/* GameTitleRankersWrapper 하나씩 추가하면 column 하나 생김 */}
+          <GameTitleRankersWrapper>
+            <GameTitle>UFO GAME</GameTitle>
+            <RankingEachGame>
+              {/* index를 key로 사용하면 비효율적, 일단 플래그가 없어서 써둠 */}
+              {!rankersOnUFOGame ? (
+                <Loading>
+                  Loading ...
+                  <LoadingImg />
+                </Loading>
+              ) : (
+                rankersOnUFOGame?.map((elem, index) => {
+                  return (
+                    <Rankers key={index}>
+                      {`${index + 1}등 ${elem[0]}`}
+                      <Score>{`- ${elem[1]}점 `}</Score>
+                    </Rankers>
+                  );
+                })
+              )}
+            </RankingEachGame>
+          </GameTitleRankersWrapper>
+
+          {/* GameTitleRankersWrapper 하나씩 추가하면 column 하나 생김 */}
+          <GameTitleRankersWrapper>
+            <GameTitle>위치를 찾아라!</GameTitle>
+            <RankingEachGame>
+              {!rankersOnGeoguesser ? (
+                <Loading>
+                  Loading ...
+                  <LoadingImg />
+                </Loading>
+              ) : (
+                rankersOnGeoguesser?.map((elem, index) => {
+                  return (
+                    <Rankers key={index}>
+                      {`${index + 1}등 ${elem[0]}`}
+                      <Score>{`- ${elem[1]}점 `}</Score>
+                    </Rankers>
+                  );
+                })
+              )}
+            </RankingEachGame>
+          </GameTitleRankersWrapper>
+
+          <GameTitleRankersWrapper>
+            <GameTitle>냥이와 끝말잇기</GameTitle>
+            <RankingEachGame>
+              {!rankersOnCatGame ? (
+                <Loading>
+                  Loading ...
+                  <LoadingImg />
+                </Loading>
+              ) : (
+                rankersOnCatGame?.map((elem, index) => {
+                  return (
+                    <Rankers key={index}>
+                      {`${index + 1}등 ${elem[0]}`}
+                      <Score>{`- ${elem[1]}점 `}</Score>
+                    </Rankers>
+                  );
+                })
+              )}
+            </RankingEachGame>
+          </GameTitleRankersWrapper>
+
+          <GameTitleRankersWrapper>
+            <GameTitle>소행성을 부숴라!</GameTitle>
+            <RankingEachGame>
+              {!rankersOnAsteroids ? (
+                <Loading>
+                  Loading ...
+                  <LoadingImg />
+                </Loading>
+              ) : (
+                rankersOnAsteroids?.map((elem, index) => {
+                  return (
+                    <Rankers key={index}>
+                      {`${index + 1}등 ${elem[0]}`}
+                      <Score>{`- ${elem[1]}점 `}</Score>
+                    </Rankers>
+                  );
+                })
+              )}
+            </RankingEachGame>
+          </GameTitleRankersWrapper>
+        </RankingEachGameWrapper>
+      </RankingWrapper>
     </Wrapper>
   );
 }
