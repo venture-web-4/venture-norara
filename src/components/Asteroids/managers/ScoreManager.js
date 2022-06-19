3ce6e95cc96a08a6e7594ba9fd9c30daa408228e
KNOXINUS <71023081+KNOXINUS@users.noreply.github.com>
2022-06-20 02:17:04 +0900

    fix: timer 관련 버그 수정

--- src/components/Asteroids/managers/ScoreManager.js
+++ src/components/Asteroids/managers/ScoreManager.js
@@ -6,8 +6,16 @@ export class ScoreManager {
     this.nowTime = new Date().getTime();
     this.timeCounter = 0;
   }
+  
+  setScoreManager() {
+    this.collectCount = 0;
+    this.timeElapsed = 0;
+    this.previousTime = new Date().getTime();
+    this.nowTime = new Date().getTime();
+    this.timeCounter = 0;
+  }
 
-  timer() {
+  runTimer() {
     this.previousTime = this.nowTime;
     this.nowTime = new Date().getTime();
     this.timeCounter += this.nowTime - this.previousTime;
